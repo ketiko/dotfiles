423b8b92b5bea35bca07af148faddc72c79eacb5
Ryan Hansen <ryan.hansen@shift.com>
2022-06-24 23:12:07 -0600

    Fixing shellcheck warnings

--- docker_testing/docker-entrypoint.sh
+++ docker_testing/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script will run anytime this docker image starts
 
-cd ~/.dotfiles
+cd ~/.dotfiles || return
 ./install dotfiles O
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
