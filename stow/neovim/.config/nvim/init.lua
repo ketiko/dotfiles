2a6c7afdd5f8fb5918a421607dbc3e7673f42820
Ryan Hansen <ryan.hansen@octanner.com>
2023-11-24 19:49:44 -0700

    test

--- stow/neovim/.config/nvim/init.lua
+++ stow/neovim/.config/nvim/init.lua
@@ -1,1 +1,12 @@
-vim.cmd('source ~/.config/nvim/old.vim')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
