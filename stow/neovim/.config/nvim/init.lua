324ce51c10841b8543675ed8ea1757dbd27b9127
Ryan Hansen <ketiko@gmail.com>
2023-11-26 03:05:14 -0700

    fixes

--- stow/neovim/.config/nvim/init.lua
+++ stow/neovim/.config/nvim/init.lua
@@ -411,14 +411,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
   end,
 })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, {})
+vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
 
