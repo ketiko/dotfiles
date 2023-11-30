05aaaa80c8bf7f1490a003ba4b14928d9a76cbdf
Ryan Hansen <ketiko@gmail.com>
2023-11-30 11:00:03 -0700

    Fixing mapping

--- stow/neovim/.config/nvim/init.lua
+++ stow/neovim/.config/nvim/init.lua
@@ -457,7 +457,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<S-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
   end,
