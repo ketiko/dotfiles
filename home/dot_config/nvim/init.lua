602329c663ff644f233aabae36aba6024b179fb1
Ryan Hansen <ketiko@gmail.com>
2023-12-09 23:08:37 -0700

    updates

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -82,6 +82,16 @@ require("lazy").setup(
         "MunifTanjim/nui.nvim",
         "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
       },
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = true,
+            hide_gitignored = false,
+          }
+        }
+      },
       keys = {
         {
           "<leader>n",
