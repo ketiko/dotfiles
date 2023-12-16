e6fd8701deedbe0f77de958db729649adb90d83f
Ryan Hansen <ketiko@gmail.com>
2023-12-16 12:59:12 -0700

    Rename file

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = " "
 
-require("config/options")
-require("config/mappings")
-require("config/lazy")
+require("config")
 
 vim.cmd('source $HOME/.config/nvim/old.vim')
