81e9e4e0e238d74c2d15410e9f5b5b0eb3d206bf
Ryan Hansen <ketiko@gmail.com>
2023-12-17 16:39:09 -0700

    tweaks

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -5,7 +5,7 @@
 --- Clear search with <esc>
 vim.keymap.set({ "i", "n" }, "\\", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 
-vim.keymap.set({ "n" }, "<Left>", "<C-w><", { desc = "Resize pane left" })
-vim.keymap.set({ "n" }, "<Down>", "<C-w>+", { desc = "Resize pane down" })
-vim.keymap.set({ "n" }, "<Up>", "<C-w>-", { desc = "Resize pane up" })
-vim.keymap.set({ "n" }, "<Right>", "<C-w>>", { desc = "Resize pane right" })
+-- vim.keymap.set({ "n" }, "<Left>", "<C-w><", { desc = "Resize pane left" })
+-- vim.keymap.set({ "n" }, "<Down>", "<C-w>+", { desc = "Resize pane down" })
+-- vim.keymap.set({ "n" }, "<Up>", "<C-w>-", { desc = "Resize pane up" })
+-- vim.keymap.set({ "n" }, "<Right>", "<C-w>>", { desc = "Resize pane right" })
