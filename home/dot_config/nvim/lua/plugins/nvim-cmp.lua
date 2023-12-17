37b47f14c84c5b4511589fa2a1c37147b46f72a9
Ryan Hansen <ketiko@gmail.com>
2023-12-17 14:14:19 -0700

    tweaks

--- home/dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ home/dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -29,14 +29,14 @@ return {
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
-      local luasnip = require("luasnip")
-
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
+      -- local defaults = require("cmp.config.default")()
+      -- local luasnip = require("luasnip")
+      --
+      -- local has_words_before = function()
+      --   unpack = unpack or table.unpack
+      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      -- end
 
       return {
         -- mapping = cmp.mapping.preset.insert({
