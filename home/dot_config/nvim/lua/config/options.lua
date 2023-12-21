909271e8374174cf620b4a06636e70aa5e6fa309
Ryan Hansen <ketiko@gmail.com>
2023-12-20 23:32:09 -0700

    Tweaks

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -2,4 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.clipboard = "" -- don't use system clipboard so that that change inside blah don't override what's in the clipboard
+vim.opt.clipboard = "" -- don't use unnamedplus so that that cut commands won't override what's in the clipboard
+vim.opt.shortmess:append({ s = true }) -- don't show "search hit BOTTOM, continuing at TOP"
---
+++ home/dot_config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    optional = true,
+    opts = {
+      presets = {
+        bottom_search = false,
+        command_palette = false,
+      },
+    },
+  },
+}
