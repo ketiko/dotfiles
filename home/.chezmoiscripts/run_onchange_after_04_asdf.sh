8a1ea56897bc37ff61b5fa2565d98c4605860165
Ryan Hansen <ketiko@gmail.com>
2024-01-22 12:48:16 -0700

    Tweaks

--- home/.chezmoiscripts/run_onchange_after_04_asdf.sh
+++ home/.chezmoiscripts/run_onchange_after_04_asdf.sh
@@ -16,20 +16,20 @@ source "$HOME/.asdf/completions/asdf.bash"
 
 echo -e "\033[0;32m>>>>> Installing asdf plugins <<<<<\033[0m"
 
-test -d "$HOME/.asdf/plugins/golang" || asdf plugin add golang https://github.com/kennyp/asdf-golang.git
-test -d "$HOME/.asdf/plugins/helm" || asdf plugin add helm https://github.com/Antiarchitect/asdf-helm.git
-test -d "$HOME/.asdf/plugins/java" || asdf plugin add java https://github.com/halcyon/asdf-java.git
-test -d "$HOME/.asdf/plugins/gradle" || asdf plugin add gradle https://github.com/rfrancis/asdf-gradle
-test -d "$HOME/.asdf/plugins/krew" || asdf plugin add krew https://github.com/bjw-s/asdf-krew.git
-test -d "$HOME/.asdf/plugins/kubectl" || asdf plugin add kubectl https://github.com/asdf-community/asdf-kubectl.git
-test -d "$HOME/.asdf/plugins/kustomize" || asdf plugin add kustomize https://github.com/Banno/asdf-kustomize.git
-test -d "$HOME/.asdf/plugins/minikube" || asdf plugin add minikube https://github.com/alvarobp/asdf-minikube.git
-test -d "$HOME/.asdf/plugins/nodejs" || asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-test -d "$HOME/.asdf/plugins/python" || asdf plugin add python https://github.com/asdf-community/asdf-python.git
-test -d "$HOME/.asdf/plugins/ruby" || asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-test -d "$HOME/.asdf/plugins/rust" || asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
-test -d "$HOME/.asdf/plugins/sbt" || asdf plugin add sbt https://github.com/bram2000/asdf-sbt.git
-test -d "$HOME/.asdf/plugins/scala" || asdf plugin add scala https://github.com/asdf-community/asdf-scala.git
-test -d "$HOME/.asdf/plugins/terraform" || asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
+test -d "$HOME/.asdf/plugins/golang" || asdf plugin add golang
+test -d "$HOME/.asdf/plugins/helm" || asdf plugin add helm
+test -d "$HOME/.asdf/plugins/java" || asdf plugin add java
+test -d "$HOME/.asdf/plugins/gradle" || asdf plugin add gradle
+test -d "$HOME/.asdf/plugins/krew" || asdf plugin add krew
+test -d "$HOME/.asdf/plugins/kubectl" || asdf plugin add kubectl
+test -d "$HOME/.asdf/plugins/kustomize" || asdf plugin add kustomize
+test -d "$HOME/.asdf/plugins/minikube" || asdf plugin add minikube
+test -d "$HOME/.asdf/plugins/nodejs" || asdf plugin add nodejs
+test -d "$HOME/.asdf/plugins/python" || asdf plugin add python
+test -d "$HOME/.asdf/plugins/ruby" || asdf plugin add ruby
+test -d "$HOME/.asdf/plugins/rust" || asdf plugin-add rust
+test -d "$HOME/.asdf/plugins/sbt" || asdf plugin add sbt
+test -d "$HOME/.asdf/plugins/scala" || asdf plugin add scala
+test -d "$HOME/.asdf/plugins/terraform" || asdf plugin add terraform
 
 echo -e "\033[0;32m>>>>> Ending asdf <<<<<\033[0m"
