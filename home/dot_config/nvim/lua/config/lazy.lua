b4f2a780e100f16693cd075243f9bb9547261995
Ryan Hansen <ketiko@gmail.com>
2024-06-03 11:51:13 -0600

    Adding toml

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -40,6 +40,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
