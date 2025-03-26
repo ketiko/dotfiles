d1f3e6211ef72589acbe8001e19a282e679a4415
Ryan Hansen <ketiko@gmail.com>
2025-03-25 18:14:22 -0600

    updates

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -46,6 +46,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.scala" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
--- home/dot_config/nvim/lua/plugins/copilot.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,11 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    suggestion = {
-      keymap = {
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
-    },
-  },
+  -- "zbirenbaum/copilot.lua",
+  -- opts = {
+  --   suggestion = {
+  --     keymap = {
+  --       next = "<M-]>",
+  --       prev = "<M-[>",
+  --     },
+  --   },
+  -- },
 }
